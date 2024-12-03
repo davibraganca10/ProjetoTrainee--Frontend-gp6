cb0fc299250bb655a7c2d58952d62f7fa51fb601
Diego Gontijo <diegogontijo452@gmail.com>
2024-12-03 19:36:26 -0300

    criacao da pagina de feed em andamento

---
+++ DICAS.md
@@ -0,0 +1,6 @@
+# a estilizacao que fiz no header na parte de feed pode ser reutilizada.
+# criei uma variavel chamada fontAll no arquivo tailwind.config.ts --> pode ser reutilizada
+# foto da logo da unb na pasta public --> bom guardar as fotos usadas lá.
+
+----------------------------------------------------------------
+
---
+++ front-end/src/pages/feed/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const FeedDeslogado = () => {
+  return (
+    <body>
+      <header className='bg-verde_unb h-20 flex items-center justify-between px-4'>
+        <Link href="/feed" className='ml-3'> 
+          <Image src="/unb_logo.png" alt='logo unb' width={92} height={46} layout="responsive" /> 
+        </Link>
+
+        <Link href='PAGINA DE LOGIN (FALTA FAZER)' 
+        className="bg-corBotaoLogin font-fontAll text-white text-lg px-6 py-1 mr-7 border-white border-2 rounded-xl hover:bg-blue-500 hover transition-all drop-shadow-2xl">
+          Login
+        </Link>
+      </header>
+      <main>
+
+        <h1 className=''>Nossos Professores</h1>
+        <input type="text" />
+      
+        
+
+      </main>
+
+    </body>
+  )
+}
+
+export default FeedDeslogado;
