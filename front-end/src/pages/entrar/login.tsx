c238c8b9fe6384dbee4331fbc21ff45b6b52d1e7
Filipe Nunes <nunesfilipenobre@gmail.com>
2024-12-03 12:52:58 -0300

    Criacao das telas de cadastro e login

---
+++ front-end/src/pages/entrar/login.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <>
+      <div>
+        <h1>Avaliação de Professores</h1>
+      </div>
+      <div>
+        <p>inputs</p>
+      </div>
+      <div>
+        <p>botões</p>
+      </div>
+    </>
+  )
+}
+
+export default login
