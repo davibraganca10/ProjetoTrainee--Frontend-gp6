42a1658525b36336c20ce98dad6afd29acc4cc87
davibraganca10 <bragancad83@gmail.com>
2024-12-07 12:02:06 -0300

    feat: alguns modais

--- front-end/src/components/nova.publicacao.tsx
+++ front-end/src/components/nova.publicacao.tsx
@@ -1,25 +1,45 @@
 import React, { useState } from 'react';
-
+import { Modal } from './modal';
 const NovaPublicacao = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const alternar = () => setIsOpen(!isOpen);
-
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  function handleOpenModal(){
+    setModalIsOpen(!modalIsOpen)
+  }
   return (
+    <>
     <div className="relative">
       <button
-        onClick={alternar}
+        onClick={handleOpenModal}
         className="bg-corBotaoLogin font-fontAll text-white text-lg px-4 py-1 mr-2 border-white border-2 rounded-xl hover:bg-blue-500 transition-all drop-shadow-2xl w-44">
         Nova Publicação
       </button>
-
-      {isOpen && (
-        <div>
-            <h1>hehe</h1>
-        </div>
-        
-      )}
     </div>
+    <Modal isOpen={modalIsOpen} onClose={handleOpenModal}>
+    <div className='mb-6 border-b border-green-300 py-7 text-center'>
+            <div className='mt-3'>
+              <h2 className='text-3xl font-semibold text-white'>Nova publicação</h2>
+            </div>
+          </div>
+         
+          <div className="mb-4">
+            <input
+              placeholder="Nome do professor"
+              className="w-full px-4 py-3 border border-gray-300 rounded-xl text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-300"
+            />
+          </div>
+          <div className="mb-4">
+          <input
+              placeholder="Disciplina"
+              className="w-full px-4 py-3 border border-gray-300 rounded-xl text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-300"
+            />
+            </div>
+        <textarea className='bg-green-100 flex flex-center items-center justify-center w-full h-60'
+        placeholder='Escreva aqui'></textarea>
+       <div className='flex justify-center'>
+      <button className="flex justify-center w-1/3 shadow-md mt-4 py-2 bg-green-500 text-white rounded hover:bg-blue-400 transition-all" onClick={()=> console.log("Clicado avaliar")}>Publicar</button>
+      </div>
+    </Modal>
+    </>
   );
 };
 
--- front-end/src/components/postLogado.tsx
+++ front-end/src/components/postLogado.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Perfil from './perfil'
 import Image from 'next/image'
-
+import { Modal } from './modal'
 interface Dados {
     user:string
     data: string;
@@ -12,8 +12,14 @@ interface Dados {
 }
 
 
-const PostLogado: React.FC<Dados> = ({ conteudo, user, data, hora, professor, departamento }) => (
+const PostLogado: React.FC<Dados> = ({ conteudo, user, data, hora, professor, departamento }) => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+  function handleOpenModal(){
+    setModalIsOpen(!modalIsOpen)
+  }
+  return(
     <>
+    
     <div className=" w-full bg-green-300 my-5 py-5 rounded-lg">
         <div className='flex flex-col justify-center items-center text-center'>
             <div className='flex flex-row'>
@@ -31,16 +37,29 @@ const PostLogado: React.FC<Dados> = ({ conteudo, user, data, hora, professor, de
             </div>          
             <div className='mr-8 flex gap-x-3'>
                 <button onClick={()=> console.log('Editar postagem')}>
-                    <Image src='/editar.png' alt='' width={18} height={20}/>
+                    <Image src='/editar.png' alt='editar' width={18} height={20} onClick={handleOpenModal}/>
+                    <Modal isOpen={modalIsOpen} onClose={handleOpenModal}> {/*chamando o modal*/}
+                    <div className='mb-6 border-b border-green-300 py-7 text-center'>
+            <div className='mt-3'>
+              <h2 className='text-3xl font-semibold text-white'>Editar avaliação</h2>
+            </div>
+          </div>
+          <textarea className='bg-green-100 flex flex-center items-center justify-center w-full h-60'
+        placeholder='Escreva aqui'></textarea>
+        <div className='flex justify-center'>
+         
+                <button className="flex justify-center w-1/3 shadow-md mt-4 py-2 bg-green-500 text-white rounded hover:bg-blue-400 transition-all" onClick={()=> console.log("Clicado editar")}>Editar</button>
+      </div>
+                    </Modal>
                 </button>
                 <button onClick={() => console.log('Excluir postagem')}>
-                    <Image src='/lixeira.png' alt='' width={20} height={20}/>
+                    <Image src='/lixeira.png' alt='excluir' width={20} height={20}/>
                 </button>
             </div>  
         </div>
     </div>
     </>
-
-);
+  )
+};
 
 export default PostLogado;
