2d39ad40b571d0a79f9d2877b39ca8fd929cea9d
Antonio Reis <antoniolucas.reiss@gmail.com>
2024-12-08 12:29:12 -0300

    Melhorias nos Users

--- front-end/src/components/perfil.tsx
+++ front-end/src/components/perfil.tsx
@@ -10,21 +10,19 @@ interface Param {
   }
 
 const Perfil: React.FC<Param> = ({ image, name, curso, email }) => (
-  <div className="rounded-xl flex text-center px-2 ">
-    <div className="w-28 h-24 mx-auto mb-4">
+  <div className="rounded-xl ml-10">
+    <div className="w-32 h-24 ml-4 mb-9">
       <Image src={image} alt='' width={64} height={96} layout="responsive"/>   
     </div>
-    <div className='ml-10 text-center justify-center flex-col'>
-      <h2 className="text-lg font-bold left-0 font-lg text-gray-700">{name}</h2>
-      <div className='flex justify-center gap-x-2'>
-        <div className='items-center mt-3'>
-          <Image src='/predio,.png' alt='' width={16} height={20}/>
-        </div>
-        <p className="text-gray-800 mt-2 mb-2 text-sm">{curso}</p>
+    <div className='mt-2 ml-5'>
+      <h2 className="text-lg font-bold font-lg text-gray-700">{name}</h2>
+      <div className='items-center flex gap-x-2'>
+        <Image className='mt-2' src='/predio,.png' alt='' width={16} height={20}/>
+        <p className="text-gray-800 mt-2 text-md">{curso}</p>
       </div>
-      <div className='items-center justify-center flex gap-x-2'>
-      <Image className='mt-2 ml-1' src='/carta.png' alt='' height={20} width={18} />
-      <p className='text-gray-800 mt-1 text-sm'>{email}</p>
+      <div className='items-center flex gap-x-2'>
+        <Image className='mt-2 ' src='/carta.png' alt='' height={20} width={18} />
+        <p className='text-gray-800 mt-1 text-sm'>{email}</p>
       </div>
     </div>
   </div>
--- front-end/src/pages/user/index.tsx
+++ front-end/src/pages/user/index.tsx
@@ -7,22 +7,23 @@ import Post from '@/components/posts'
 export default function UserDeslogado(){
   return <main>
     <Header />
-      <div className="max-w-2xl mx-auto p-6 bg-white border border-gray-300 rounded-lg shadow-md">
-        <div className='w-full'>
-          <div className='bg-green-100 float-left w-full rounded py-3'>
-            <div className='ml-5 float-left'>
-              <Perfil
-                image='/morty.png'
-                name='Morty  Gamer'
-                curso='Ciencia da Computação'
-                email='morty.gamer@cjr.org.br'
-              >
-              </Perfil>
+      <div className="max-w-xl mx-auto bg-white border border-gray-300 rounded-lg shadow-md">
+      <div className='flex flex-col items-center justify-center justify-between'>
+            <div className='w-full border-2 border-gray-200'>
+              <div className='h-32 float-top bg-green-400 rounded py-10 w-full'>
+              <div className='flex flex-col ml-4 float-left'>
+                {/*criei o component perfil pra colocar a imagem e os dados do user */}
+                <div className='py-4'><Perfil
+                  image="/morty.png"
+                  name="Morty Gamer"  
+                  curso="Ciencia da Computação"
+                  email="morty.gamer@cjr.org.br">
+                </Perfil> 
+              </div></div>
             </div>
           </div>
-        </div>
         <hr className="flex justify-center items-center w-2xl "></hr>
-        <div className='grid place-items-center w-full'>
+        <div className='grid place-items-center w-full p-4'>
           {/*Criei o Post para ver as postagens*/}
           <Post
             user='Billy Batson'
@@ -49,6 +50,7 @@ export default function UserDeslogado(){
             conteudo='Avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário avaliação 2 desse usuário'>
           </Post>
         </div>
+      </div>
       </div>
   </main>
 }
