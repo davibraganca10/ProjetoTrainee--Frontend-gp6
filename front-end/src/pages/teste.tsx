14b53765c5eb1be38be2c4ab0329be8ebfb18bb7
Filipe Nunes <nunesfilipenobre@gmail.com>
2024-12-11 12:21:19 -0300

    testando axios

---
+++ front-end/src/pages/teste.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { getUsers } from '../utils/api'
+
+const teste = () => {
+
+    const getUsers = async() =>{
+        try{
+            await getUsers()
+        }
+        catch{
+            
+        }
+    }
+  return (
+    <div>DIACK</div>
+  )
+}
+
+export default teste
--- front-end/src/utils/api.ts
+++ front-end/src/utils/api.ts
@@ -5,6 +5,19 @@ const api = axios.create({
 })
 
 
-const getUser = async() => {
-    await api.get("/user/");
+export const getUsers = async() => {
+    const response = await api.get("/user");
+    return response.data;
+}
+
+export const CreateUser = async (user: Partial<User>) => {
+    const response = await api.post("/user",{
+        name: user.name,
+        email: user.email
+    });
+    return response.data;
+}
+
+export const patchUserReq = async(user: Partial<User>, id:number) => {
+    api.patch('/user${id}',user);
 }
