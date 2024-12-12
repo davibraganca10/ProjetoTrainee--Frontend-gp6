b8771949152a823f33e11afacec54f59f5107689
Diego Gontijo <diegogontijo452@gmail.com>
2024-12-12 20:17:06 -0300

    card

--- front-end/src/components/card_prof.tsx
+++ front-end/src/components/card_prof.tsx
@@ -10,15 +10,16 @@ interface Parametros {
   }
 
 const Card: React.FC<Parametros> = ({id, image, name, role }) => (
-    <div className="w-38 h-46 bg-white rounded-xl shadow-lg p-4 text-center overflow-hidden">
+  <div className="w-38 h-46 bg-white rounded-xl shadow-lg p-4 text-center overflow-hidden">
+      <Link href={`/avaliacao_com_comentario/${id}`}>
       <div className="w-28 h-24 mx-auto mb-4">
         <Image src={image} alt={name} width={64} height={96} layout="responsive"/>
         
       </div>
-      <Link href={`/avaliacao_com_comentario/${id}`}>
+      
       <h2 className="text-lg font-bold text-gray-800">{name}</h2>
-      </Link>
       <p className="text-gray-500 text-sm">{role}</p>
+      </Link>
     </div>
   );
 
