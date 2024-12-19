cfea1c772376a89e090f35fa1286ee335e25ebab
davibraganca10 <bragancad83@gmail.com>
2024-12-19 17:58:41 -0300

    feat: pag user deslogado integrada

--- front-end/src/components/posts.tsx
+++ front-end/src/components/posts.tsx
@@ -4,21 +4,20 @@ import Image from 'next/image'
 
 interface Dados {
   user:string | null;
-  data: string | null;
-  hora: string | null;
-  professor: string | null;
-  departamento: string | null;
-  conteudo: string | null;
+  datahora: string
+  professor: string | undefined;
+  departamento: string | undefined;
+  conteudo: string;
 }
 
 
-const Post: React.FC<Dados> = ({ conteudo, user, data, hora, professor, departamento }) => (
+const Post: React.FC<Dados> = ({ conteudo, user, datahora, professor, departamento }) => (
   <>
     <div className=" w-full bg-green-300 my-5 py-5 rounded-lg">
       <div className='flex flex-col justify-center items-center text-center'>
         <div className='flex flex-row'>
           <h1 className='text'>{user}</h1>
-          <div className='text-gray-500 ml-2 float-right overflow-hidden overflow-y-auto' >{data} - {hora} - {professor} - {departamento}</div>
+          <div className='text-gray-500 ml-2 float-right overflow-hidden overflow-y-auto' >{datahora} - {professor} - {departamento}</div>
         </div>
         <br></br>
         <p className='text-sm ml-5 mr-5'>{conteudo}</p>
